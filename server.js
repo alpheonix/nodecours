f0a5232bf927e17366bf69319f07dacee92c24f5
alpheonix <arthurblanc98@gmail.com>
2019-03-11 15:31:46 +0100

    post request

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
 const port = 3000;
+console.log("Hello World");
 
 app.get('/hello', (req, res) => {    
     res.send('hello world');
+  });
+
+  app.use(express.json());
+
+  app.post('/chat', (req, res) => {
+    const request = req.body;
+      if(request.msg == "ville"){
+        res.send('Nous sommes à Paris');
+      }else if(request.msg == "meteo"){
+        res.send('Il fait beau');
+      }
   });
 
   app.listen(process.env.PORT || port,() => {
