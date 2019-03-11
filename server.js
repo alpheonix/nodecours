be2dbc568d04b072880ad45c98817716dc40e81a
alpheonix <arthurblanc98@gmail.com>
2019-03-11 12:45:09 +0100

    path change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {    
+app.get('/', (req, res) => {    
     res.send('hello world');
   });
 
